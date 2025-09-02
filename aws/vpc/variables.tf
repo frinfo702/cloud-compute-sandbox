2fcd3ccf675838fe999183a0de915d5ef73f60f0
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2025-09-02 17:40:05 +0900

    terraform fmt

--- aws/vpc/variables.tf
+++ aws/vpc/variables.tf
@@ -1,14 +1,14 @@
-variable "cidr_block" { 
-description = "CIDR block for the VPC"
-        type = string 
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
 
-        validation {
-            condition     = can(cidrhost(var.cidr_block, 0))
-            error_message = "Must be valid IPv4 or IPv6 CIDR."
-        }
-    }
+  validation {
+    condition     = can(cidrhost(var.cidr_block, 0))
+    error_message = "Must be valid IPv4 or IPv6 CIDR."
+  }
+}
 
 variable "name" {
-        description = "Name tag for the VPC"
-        type = string 
-     }
+  description = "Name tag for the VPC"
+  type        = string
+}
