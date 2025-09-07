926977052365e3846165b64cb98c0c48a800e60f
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2025-09-07 17:58:41 +0900

    Refactor AMI retrieval in AWS instance module to use SSM parameter for best practices

--- aws/aws_instance/main.tf
+++ aws/aws_instance/main.tf
@@ -1,20 +1,10 @@
-data "aws_ami" "amazon_linux_2023" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-hvm-*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
+# best practice of retreaving AMI
+data "aws_ssm_parameter" "amazon_linux_2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
 }
 
 resource "aws_instance" "web_server" {
-  ami                         = data.aws_ami.amazon_linux_2023.id
+  ami                         = data.aws_ssm_parameter.amazon_linux_2023.value
   instance_type               = var.instance_type
   key_name                    = var.key_name
   vpc_security_group_ids      = var.vpc_security_group_ids
